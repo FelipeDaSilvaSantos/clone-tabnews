0c9c7eabf8b888aa11c2e4ef1ddb42e7aebe8f93
Felipe Silva <92223330+FelipeDaSilvaSantos@users.noreply.github.com>
2024-03-02 22:56:59 +0000

    modified sentence with 'phrase of the day'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home () {
-    return <h1>“As is a tale, so is life: not how long it is, but how good it is, is what matters.” ― Seneca</h1>
+    return <h1>Phrase of the day: “As is a tale, so is life: not how long it is, but how good it is, is what matters.” ― Seneca</h1>
 }
 
 export default home;
