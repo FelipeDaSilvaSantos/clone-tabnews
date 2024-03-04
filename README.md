05367e5450617eff030bcd6dc23d65916cc610ea
Felipe Silva <92223330+FelipeDaSilvaSantos@users.noreply.github.com>
2024-03-04 17:08:59 +0000

    adds `link:check` and `link:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone of the tabnews by Filipe Deschamps from curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function home () {
-    return <h1>Phrase of the day: “As is a tale, so is life: not how long it is, but how good it is, is what matters.” ― Seneca</h1>
+function home() {
+  return (
+    <h1>
+      Phrase of the day: “As is a tale, so is life: not how long it is, but how
+      good it is, is what matters.” ― Seneca
+    </h1>
+  );
 }
 
 export default home;
